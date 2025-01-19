ca293ae5f9803cecdc9ef887d90dd21c5e5dde82
emresen20 <emrebernarn@gmail.com>
2025-01-19 12:41:26 +0300

    courceses query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 
 const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLSchema,GraphQLList}= require('graphql')
-///denemepush
+
 const {kurslar,egitmenler}=require('../ornekveri')
 
 const EgitmenType=new GraphQLObjectType({
@@ -12,16 +12,26 @@ const EgitmenType=new GraphQLObjectType({
     })
 })
 
+const KursType=new GraphQLObjectType({
+    name:'Kurs',
+    fields:()=>({
+        id:{type:GraphQLID},
+        isim:{type:GraphQLString},
+        aciklama:{type:GraphQLString},
+        durum:{type:GraphQLString}
+    })
+})
 
-const RootQuery = new GraphQLObjectType({
+
+const RootQuery = new GraphQLObjectType({ 
     name: 'RootQueryType',
     fields: {
       egitmen: {
         type: EgitmenType,
         args: { id: { type: GraphQLID } },
         resolve(parent, args) {
-          // `args.id` string olduğu için sayıya çeviriyoruz
-          const id = parseInt(args.id);
+         
+          const id = parseInt(args.id);  // `args.id` string olduğu için sayıya çeviriyoruz
           return egitmenler.find((egitmen) => egitmen.id === id);
         },
       },
@@ -29,6 +39,20 @@ const RootQuery = new GraphQLObjectType({
         type:new GraphQLList(EgitmenType),
         resolve(parent,args){
             return egitmenler
+        }
+      },
+      kurs:{
+        type:KursType,
+        args: {id: {type:GraphQLID}},
+        resolve(parent,args){
+            const id = parseInt(args.id);
+            return kurslar.find(kurs=>kurs.id===id)
+        }
+      },
+      kurslar:{
+        type:new GraphQLList(KursType),
+        resolve(parent,args){
+            return kurslar
         }
       }
     },
